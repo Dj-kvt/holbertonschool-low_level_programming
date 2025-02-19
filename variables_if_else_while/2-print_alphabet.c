797149c723f63e82d8b9184af2f01b781db8a27e
Dj-kvt <vyckash.william.tresor@gmail.com>
2025-02-19 14:14:00 +0000

    change all my main program

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+	char c = 'a';
 
-	char c;
-	for (c = 'a' ; c <= 'z' ; c++)
-		putchar (c);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
 
 	return (0);
-
 }
 
