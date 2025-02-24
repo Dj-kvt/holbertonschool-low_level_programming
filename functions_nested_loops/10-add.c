a9aea4c8e5f9fba051763eecbc69c993451414b3
Dj-kvt <vyckash.william.tresor@gmail.com>
2025-02-24 21:59:11 +0000

    add the file 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Ajoute deux entiers et retourne le résultat
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: Somme de a et b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
