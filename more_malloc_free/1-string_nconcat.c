8505f4fb74ae31a1f8b89778b4069da2070d24b2
Dj-kvt <vyckash.william.tresor@gmail.com>
2025-03-11 13:44:28 +0000

    create the directory more malloc_free and add the file 0 and 1 with all the modification of m main.h

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings up to n bytes of s2
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes to take from s2
+ *
+ * Return: Pointer to the newly allocated string, or NULL if fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    unsigned int len1 = 0, len2 = 0, i, j;
+    char *new_str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n > len2)
+		n = len2;
+
+
+	new_str = malloc(len1 + n + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		new_str[i + j] = s2[j];
+
+	new_str[i + j] = '\0';
+
+	return (new_str);
+}
+
---
+++ more_malloc_free/README.md
@@ -0,0 +1,6 @@
+## using more fonctionality of malloc and free
+
+#-0. Trust no one: 0-malloc_checked.c
+#-1. string_nconcat: 1-string_nconcat.c
+#-2. _calloc: 2-calloc.c
+#-3. array_range: 3-array_range.c
